4a26d6d6630fb860bb09c1bd6c3d1b90dfcf2b6b
Joana Freitas <joanafreitas13@gmail.com>
2023-05-28 11:09:26 +0200

    Modificar Libro

---
+++ app/src/main/java/com/svalero/bookApp/contract/ModifyBookContract.java
@@ -0,0 +1,23 @@
+package com.svalero.bookApp.contract;
+
+import com.svalero.bookApp.domain.Book;
+
+public interface ModifyBookContract {
+
+    interface Model {
+        interface OnModifyBookListener { //Creamos un listener para devolver el User modificado  si xtodo va bien o el error si va mal
+            void onModifySuccess(Book book);
+            void onModifyError(String message);
+        }
+        void modifyBook(long id, Book book, ModifyBookContract.Model.OnModifyBookListener listener);
+    }
+
+    interface View {
+        void showError(String errorMessage);
+        void showMessage(String message);
+    }
+
+    interface Presenter {
+        void modifyBook(long id, Book book);
+    }
+}
---
+++ app/src/main/java/com/svalero/bookApp/view/ModifyBookView.java
@@ -0,0 +1,14 @@
+package com.svalero.bookApp.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ModifyBookView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modify_book_view);
+    }
+}
